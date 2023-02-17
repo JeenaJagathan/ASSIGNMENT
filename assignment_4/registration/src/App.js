fbfe974a86b0bd34250710d32542f2ebffa3ba2e
JeenaJagathan <124899021+JeenaJagathan@users.noreply.github.com>
2023-02-17 15:36:39 +0530

    formatted

--- assignment_4/registration/src/App.js
+++ assignment_4/registration/src/App.js
@@ -1,13 +1,13 @@
 // import logo from './logo.svg';
-import './App.css';
-import './components/images/icon.jpg';
-import './components/Registration.css'
-import Registration from './components/Registration.js';
+import "./App.css";
+import "./components/images/icon.jpg";
+import "./components/Registration.css";
+import Registration from "./components/Registration.js";
 function App() {
   return (
     <div className="App">
-                <Registration/>
-            </div>
+      <Registration />
+    </div>
   );
 }
 
