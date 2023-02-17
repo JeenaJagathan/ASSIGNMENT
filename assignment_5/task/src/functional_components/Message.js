c9151dc5dfc056dbba946cc9b0cd356e564860f8
JeenaJagathan <124899021+JeenaJagathan@users.noreply.github.com>
2023-02-17 15:33:29 +0530

    formatted

--- assignment_5/task/src/functional_components/Message.js
+++ assignment_5/task/src/functional_components/Message.js
@@ -1,15 +1,14 @@
 import React from "react";
 import { useState } from "react";
 function Message(props) {
-const { question: qn,  onbuttonclick } = props;
+  const { question: qn, onbuttonclick } = props;
   const buttonclicked = () => {
     onbuttonclick();
-    setsAns("Tiger is the national animal")
+    setsAns("Tiger is the national animal");
   };
-const [sAns, setsAns] = useState(qn);
+  const [sAns, setsAns] = useState(qn);
   return (
     <div>
-     
       <h1>{sAns}</h1>
       <button onClick={buttonclicked}>Click Me</button>
     </div>
--- assignment_5/task/src/functional_components/Parent.js
+++ assignment_5/task/src/functional_components/Parent.js
@@ -5,10 +5,17 @@ function Parent() {
   const buttonClick = () => {
     console.log("answer viewed");
   };
-  return (<div>
-     <Message question="Name the national animal of INDIA?" onbuttonclick={buttonClick} />
-     <Message question="Name the national bird of Bangladesh?"onbuttonclick={buttonClick}  />
-     </div>
+  return (
+    <div>
+      <Message
+        question="Name the national animal of INDIA?"
+        onbuttonclick={buttonClick}
+      />
+      <Message
+        question="Name the national bird of Bangladesh?"
+        onbuttonclick={buttonClick}
+      />
+    </div>
   );
 }
 export default Parent;
