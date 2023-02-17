d0f84cb8428a2f2af01c4dea6e1d79dee7a49fa5
Jeena Jagathan(UST <226714@ust-global.com>
2023-02-17 15:03:33 +0530

    react reg page

---
+++ assignment_4/registration/src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import './App.css';
+import './components/images/icon.jpg';
+import './components/Registration.css'
+import Registration from './components/Registration.js';
+function App() {
+  return (
+    <div className="App">
+                <Registration/>
+            </div>
+  );
+}
+
+export default App;
